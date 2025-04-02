ac8d1bd0b58d5abfd927b8e487d132c37f5f5ed8
Haidi11 <39265042+Haidi11@users.noreply.github.com>
2025-04-02 15:13:03 +0200

    Update styles, header, icons, content, and include location-selector component.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/angular/standalone';
+import { LocationSelectorComponent } from 'src/app/components/location-selector/location-selector-component.component';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, LocationSelectorComponent],
 })
+
 export class HomePage {
   constructor() {}
 }
+
