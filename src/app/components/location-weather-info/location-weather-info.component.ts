8b90c416374cb2546fe7dc47fbc575b71d8d341a
Haidi11 <39265042+Haidi11@users.noreply.github.com>
2025-04-02 14:44:57 +0200

    Create styles, layout, logic and dummy data for location-weather-info component.

---
+++ src/app/components/location-weather-info/location-weather-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-location-weather-info',
+  templateUrl: './location-weather-info.component.html',
+  styleUrls: ['./location-weather-info.component.scss'],
+})
+export class LocationWeatherInfoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
