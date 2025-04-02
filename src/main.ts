b642affd586a7cd89373fdf908df465456231c6a
Haidi11 <39265042+Haidi11@users.noreply.github.com>
2025-04-02 15:16:26 +0200

    Add chevron icons to main file.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons'; 
+import { chevronDown, chevronForward } from 'ionicons/icons';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+addIcons({
+  chevronDown, chevronForward
+});
 
 bootstrapApplication(AppComponent, {
   providers: [
