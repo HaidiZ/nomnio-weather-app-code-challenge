f1c42f640e298b24a7eb557f39375ec6862574ff
Haidi11 <39265042+Haidi11@users.noreply.github.com>
2025-04-03 19:48:21 +0200

    Create weather actions, recuers, effects and selectors.

---
+++ src/app/store/selectors/weather.selectors.ts
@@ -0,0 +1,24 @@
+import { createSelector, createFeatureSelector } from '@ngrx/store';
+import { WeatherState } from '../reducers/weather.reducer';
+
+export const selectWeatherState = createFeatureSelector<WeatherState>('weather');
+
+export const selectWeatherData = createSelector(
+    selectWeatherState,
+    (state) => state.weatherData
+);
+
+export const selectWeatherIconUrl = createSelector(
+    selectWeatherState,
+    (state) => state.iconUrl
+);
+
+export const selectWeatherLoading = createSelector(
+    selectWeatherState,
+    (state) => state.loading
+);
+
+export const selectWeatherError = createSelector(
+    selectWeatherState,
+    (state) => state.error
+);
