560dd8ef0997da6912d13acd7defb2160109d2ab
Haidi11 <39265042+Haidi11@users.noreply.github.com>
2025-04-04 13:30:41 +0200

    Add TranslationService to app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, TranslateModule],
+  providers: [TranslateService]
 })
 export class AppComponent {
   constructor() {}
