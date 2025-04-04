b9c70fdcc3302921f2e287256b5228fb359309e7
Haidi11 <39265042+Haidi11@users.noreply.github.com>
2025-04-04 23:55:02 +0200

    Format location service, effects and reducers

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -2,39 +2,37 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class LocationService {
 
-    constructor() { }
+  constructor() { }
 
-    getCurrentLocation(): Observable<any> {
-        return new Observable(observer => {
-            // Geocolation je blokiran za localhost, zato sem hardcodala,
-            // sem pa pustila kodo, da je viden moj pristop. :)
-            /*
-            if (navigator.geolocation) {
-              navigator.geolocation.getCurrentPosition(
-                (position) => {
-                  const { latitude, longitude } = position.coords;  
-                  observer.next({ latitude, longitude });
-                  observer.complete();
-                },
-                (error) => {
-                  console.error('Error getting location:', error);
-                  observer.error(error);
-                }
-              );
-            } else {
-              observer.error('Geolocation is not supported by this browser.');
-            }
-            */
-            const fallbackLat = 46.4199;
-            const fallbackLng = 15.8705;
-            observer.next({ latitude: fallbackLat, longitude: fallbackLng });
+  getCurrentLocation(): Observable<any> {
+    return new Observable(observer => {
+      // Geocolation je blokiran za localhost, zato sem hardcodala,
+      // sem pa pustila kodo, da je viden moj pristop. :)
+      /*
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            observer.next({ latitude, longitude });
             observer.complete();
-        });
-    }
-
-
+          },
+          (error) => {
+            console.error('Error getting location:', error);
+            observer.error(error);
+          }
+        );
+      } else {
+        observer.error('Geolocation is not supported by this browser.');
+      }
+      */
+      const fallbackLat = 46.4199;
+      const fallbackLng = 15.8705;
+      observer.next({ latitude: fallbackLat, longitude: fallbackLng });
+      observer.complete();
+    });
+  }
 }
--- src/app/store/effects/location.effects.ts
+++ src/app/store/effects/location.effects.ts
@@ -10,47 +10,47 @@ import { LocationService } from 'src/app/services/location.service';
 @Injectable()
 export class LocationEffects {
 
-    constructor(
-        private actions$: Actions,
-        private weatherService: WeatherService,
-        private locationService: LocationService
-    ) { }
+  constructor(
+    private actions$: Actions,
+    private weatherService: WeatherService,
+    private locationService: LocationService
+  ) {}
 
-    fetchCurrentLocation$ = createEffect(() =>
-        this.actions$.pipe(
-            ofType(fetchCurrentLocation),
-            switchMap(() =>
-                this.locationService.getCurrentLocation().pipe(
-                    map(({ latitude, longitude }) => {
-                        return setLocation({ latitude, longitude });
-                    }),
-                    catchError((error) => {
-                        console.error('Error fetching location:', error);
-                        return of(setLocation({ latitude: null, longitude: null }));
-                    })
-                )
-            )
+  fetchCurrentLocation$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(fetchCurrentLocation),
+      switchMap(() =>
+        this.locationService.getCurrentLocation().pipe(
+          map(({ latitude, longitude }) => {
+            return setLocation({ latitude, longitude });
+          }),
+          catchError((error) => {
+            console.error('Error fetching location:', error);
+            return of(setLocation({ latitude: null, longitude: null }));
+          })
         )
-    );
+      )
+    )
+  );
 
-    fetchWeatherForCurrentLocation$ = createEffect(() =>
-        this.actions$.pipe(
-            ofType(setLocation),
-            switchMap(({ latitude, longitude }) => {
-                if (latitude && longitude) {
-                    return this.weatherService.getWeatherByCoordinates(latitude, longitude).pipe(
-                        map((weatherData) => {
-                            return fetchWeather({ location: weatherData.name });
-                        }),
-                        catchError((error) => {
-                            console.error('Error fetching weather:', error);
-                            return of(fetchWeather({ location: 'Error' }));
-                        })
-                    );
-                } else {
-                    return of(fetchWeather({ location: 'Error' }));
-                }
+  fetchWeatherForCurrentLocation$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(setLocation),
+      switchMap(({ latitude, longitude }) => {
+        if (latitude && longitude) {
+          return this.weatherService.getWeatherByCoordinates(latitude, longitude).pipe(
+            map((weatherData) => {
+              return fetchWeather({ location: weatherData.name });
+            }),
+            catchError((error) => {
+              console.error('Error fetching weather:', error);
+              return of(fetchWeather({ location: 'Error' }));
             })
-        )
-    );
+          );
+        } else {
+          return of(fetchWeather({ location: 'Error' }));
+        }
+      })
+    )
+  );
 }
